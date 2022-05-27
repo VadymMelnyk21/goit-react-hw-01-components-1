5a1756ef22c1848a407888479181ca5568018282
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-27 23:37:52 +0300

    profile base

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@
 import Profile from 'components/profile/Profile';
 
 // імпорт json
-import user from './data/user.json';
-import data from './data/data.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
+// import user from './data/user.json';
+// import data from './data/data.json';
+// import friends from './data/friends.json';
+// import transactions from './data/transactions.json';
 
 export default function App() {
   return (
     <div>
-      
+      <Profile />
     </div>
   );
 };
