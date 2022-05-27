32e95db5ed0905a31189bc2655b5b36a2c22e4e5
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-27 23:14:29 +0300

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-export const App = () => {
+// імпорт компонентів
+import Profile from 'components/profile/Profile';
+
+// імпорт json
+import user from './data/user.json';
+import data from './data/data.json';
+import friends from './data/friends.json';
+import transactions from './data/transactions.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      
     </div>
   );
 };
