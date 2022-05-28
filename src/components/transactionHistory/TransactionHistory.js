7103b53a5e443563eaa6efa9f1c4f40a775aedaf
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 21:20:06 +0300

    trans markup

--- src/components/transactionHistory/TransactionHistory.js
+++ src/components/transactionHistory/TransactionHistory.js
@@ -1,27 +1,27 @@
 // import PropTypes from 'prop-types';
 // import s from './FriendList.module.css';
 
-export default function TransactionHistory() { }
+export default function TransactionHistory({ items }) {
+    return (
+        <table class="transaction-history">
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+            </thead>
 
-<table class="transaction-history">
-    <thead>
-        <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-        </tr>
-    </thead>
-
-    <tbody>
-        <tr>
-            <td>Invoice</td>
-            <td>125</td>
-            <td>USD</td>
-        </tr>
-        <tr>
-            <td>Withdrawal</td>
-            <td>85</td>
-            <td>USD</td>
-        </tr>
-    </tbody>
-</table>
+            <tbody>
+                {items.map(({ id, type, amount, currency }) => (
+                    <tr key={id}>
+                        <td>{type}</td>
+                        <td>{amount}</td>
+                        <td>{currency} </td>
+                    </tr>
+                ))
+                }
+            </tbody>
+        </table>
+    )
+}
