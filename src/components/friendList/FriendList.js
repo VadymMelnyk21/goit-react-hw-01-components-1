93d59137ce458f5f9036a8a34b9081613aedd499
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 20:04:51 +0300

    friendsList jsx

--- src/components/friendList/FriendList.js
+++ src/components/friendList/FriendList.js
@@ -2,10 +2,19 @@
 import FriendListItem from 'FriendListItem'
 import s from './FriendList.module.css';
 
-export default function FriendLis() {
+export default function FriendLis({ friends }) {
     return (
-        <ul className={s.friendList} >
-            <FriendListItem />
+        < ul className={s.friendList} >
+            {
+                friends.map((id, avatar, name, isOnline) => (
+                    <FriendListItem
+                        key={id}
+                        avatar={avatar}
+                        name={name}
+                        isOnline={isOnline}
+                    />
+                ))
+            }
         </ul >
     )
 };
--- src/components/friendList/FriendListItem.js
+++ src/components/friendList/FriendListItem.js
@@ -1,8 +1,12 @@
 // import PropTypes from 'prop-types';
 import s from './FriendList.module.css';
 
-<li class="item">
-    <span class="status"></span>
-    <img class="avatar" src="" alt="User avatar" width="48" />
-    <p class="name"></p>
-</li>
+export default function FriendListItem({ id, avatar, name, isOnline }) {
+    return (
+        <li className={s.item} key={id}>
+            <span className={s.status}></span>
+            <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
+            <p className={s.name}>{name}</p>
+        </li>
+    )
+}
