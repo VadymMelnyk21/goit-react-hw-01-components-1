c0afa99d41c87bf9596c1720111ae433b0f0c490
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 18:38:06 +0300

    statistic start

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 // імпорт компонентів
 import Profile from 'components/profile/Profile';
+import Statistics from 'components/statistics/Statistics';
 
 // імпорт json
 import user from './data/user.json';
-// import data from './data/data.json';
+import data from './data/data.json';
 // import friends from './data/friends.json';
 // import transactions from './data/transactions.json';
 
@@ -17,6 +18,10 @@ export default function App() {
       avatar={user.avatar}
       stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      
     </div>
   );
 };
