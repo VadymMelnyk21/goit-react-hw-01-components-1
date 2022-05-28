805264b08f6bb3dafce8f573a7d92f860a2f3a60
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 20:16:05 +0300

    friend statys

--- src/components/friendList/FriendList.js
+++ src/components/friendList/FriendList.js
@@ -1,12 +1,12 @@
 // import PropTypes from 'prop-types';
-import FriendListItem from 'FriendListItem'
+import FriendListItem from './FriendListItem'
 import s from './FriendList.module.css';
 
 export default function FriendLis({ friends }) {
     return (
         < ul className={s.friendList} >
             {
-                friends.map((id, avatar, name, isOnline) => (
+                friends.map(({ id, avatar, name, isOnline }) => (
                     <FriendListItem
                         key={id}
                         avatar={avatar}
--- src/components/friendList/FriendListItem.js
+++ src/components/friendList/FriendListItem.js
@@ -4,7 +4,8 @@ import s from './FriendList.module.css';
 export default function FriendListItem({ id, avatar, name, isOnline }) {
     return (
         <li className={s.item} key={id}>
-            <span className={s.status}></span>
+            <span className={isOnline ? 'online' : 'offlone'}
+            ></span>
             <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={s.name}>{name}</p>
         </li>
