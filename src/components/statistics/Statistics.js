9447b0db44d8de051d7e2008f41e5c8df55a3aac
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 19:01:17 +0300

    statistic propTypes

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,4 +1,4 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
 export default function Statistics({ title, stats }) {
@@ -17,4 +17,11 @@ export default function Statistics({ title, stats }) {
             </ul>
         </section>
     )
-}
+};
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    id: PropTypes.string,
+    label: PropTypes.string,
+    percentage: PropTypes.number,
+};
