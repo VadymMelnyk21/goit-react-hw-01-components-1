dca4e6202e4e20faf2f1e1625dd3e2f003d26266
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 20:21:44 +0300

    friend list propType

--- src/components/friendList/FriendListItem.js
+++ src/components/friendList/FriendListItem.js
@@ -1,13 +1,20 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import s from './FriendList.module.css';
 
 export default function FriendListItem({ id, avatar, name, isOnline }) {
     return (
         <li className={s.item} key={id}>
-            <span className={isOnline ? 'online' : 'offlone'}
+            <span className={isOnline ? 'online' : 'offline'}
             ></span>
             <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={s.name}>{name}</p>
         </li>
     )
+}
+
+FriendListItem.propTypes = {
+    id: PropTypes.number,
+    avatar: PropTypes.string,
+    name: PropTypes.string,
+    isOnline: PropTypes.bool,
 }
