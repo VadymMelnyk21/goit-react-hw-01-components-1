050b8161dbfbf71e7a0efcd4bbb4e0c5e0bef698
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 21:08:49 +0300

    transaction stert

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@
 import Profile from 'components/profile/Profile';
 import Statistics from 'components/statistics/Statistics';
 import FriendList from 'components/friendList/FriendList'
+import TransactionHistory from 'components/transactionHistory/TransactionHistory'
 
 // імпорт json
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
-// import transactions from './data/transactions.json';
+import transactions from './data/transactions.json';
 
 export default function App() {
   return (
@@ -24,6 +25,8 @@ export default function App() {
       <Statistics stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />;
       
     </div>
   );
---
+++ src/components/transactionHistory/TransactionHistory.js
@@ -0,0 +1,27 @@
+// import PropTypes from 'prop-types';
+// import s from './FriendList.module.css';
+
+export default function TransactionHistory() { }
+
+<table class="transaction-history">
+    <thead>
+        <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr>
+            <td>Invoice</td>
+            <td>125</td>
+            <td>USD</td>
+        </tr>
+        <tr>
+            <td>Withdrawal</td>
+            <td>85</td>
+            <td>USD</td>
+        </tr>
+    </tbody>
+</table>
