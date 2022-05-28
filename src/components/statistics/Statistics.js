295a400753a92f3e873d7e19241aad3c3144423f
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 18:47:49 +0300

    statistics map

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -1,28 +1,19 @@
 // import PropTypes from 'prop-types';
 import s from './Statistics.module.css';
 
-export default function Statistics() {
+export default function Statistics({ title, stats }) {
     return (
         <section className={s.statistics}>
-            <h2 className={s.title}>Upload stats</h2>
+            {title && <h2 className={s.title}>{title}</h2>}
 
             <ul className={s.statList}>
-                <li className={s.item}>
-                    <span className={s.label}>.docx</span>
-                    <span className={s.percentage}>4%</span>
-                </li>
-                <li className={s.item}>
-                    <span className={s.label}>.mp3</span>
-                    <span className={s.percentage}>14%</span>
-                </li>
-                <li className={s.item}>
-                    <span className={s.label}>.pdf</span>
-                    <span className={s.percentage}>41%</span>
-                </li>
-                <li className={s.item}>
-                    <span className={s.label}>.mp4</span>
-                    <span className={s.percentage}>12%</span>
-                </li>
+                {stats.map(stat => (
+                    <li className={s.item} key={stat.id}>
+                        <span className={s.label}>{stat.label}</span>
+                        <span className={s.percentage}>{stat.percentage}%</span>
+                    </li>
+                ))
+                }
             </ul>
         </section>
     )
