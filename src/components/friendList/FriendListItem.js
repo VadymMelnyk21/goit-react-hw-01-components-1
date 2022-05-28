dbe10aebeabacb031afbc58583ab4bdd380bc0d7
VadymMelnyk21 <91023743+VadymMelnyk21@users.noreply.github.com>
2022-05-28 20:57:02 +0300

    friends styles

--- src/components/friendList/FriendListItem.js
+++ src/components/friendList/FriendListItem.js
@@ -4,7 +4,7 @@ import s from './FriendList.module.css';
 export default function FriendListItem({ id, avatar, name, isOnline }) {
     return (
         <li className={s.item} key={id}>
-            <span className={isOnline ? 'online' : 'offline'}
+            <span className={isOnline ? (s.online) : (s.offline)}
             ></span>
             <img className={s.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={s.name}>{name}</p>
